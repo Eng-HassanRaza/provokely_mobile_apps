c6d6da32ff206b24107d41b2cc237db7589284e2
Hassan Raza <31651864+Eng-HassanRaza@users.noreply.github.com>
2025-10-03 16:00:23 +0200

    missing folder addedv3

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:8000',
+    defaultValue: 'https://b7952588dcff.ngrok-free.app',
   );
 
   static const bool enableFcm = bool.fromEnvironment(
