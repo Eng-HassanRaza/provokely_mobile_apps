e6ee97e66985e1112ffab1ccbaf19aa88cedbed8
Hassan Raza <31651864+Eng-HassanRaza@users.noreply.github.com>
2025-10-04 19:40:14 +0200

    login error fix v2

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'https://provokely.com',
+    defaultValue: 'https://provokely.com/',
   );
 
   static const bool enableFcm = bool.fromEnvironment(
